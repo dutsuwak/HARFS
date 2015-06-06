e8bae8a1d15960bb4b06138b3fe84cb3cd2b0f7f
abrahamon <Abrahamon@users.noreply.github.com>
2015-06-06 11:06:22 -0600

    ahora el servidor escucha por medio de un hilo cuando se instancia, socker server

--- src/com.HARFS.NetworkAccess/Server.cpp
+++ src/com.HARFS.NetworkAccess/Server.cpp
@@ -18,6 +18,8 @@ Server::Server(int pPort) {
 Server::~Server() {
 }
 
+/* A simple server in the internet domain using TCP
+   El puerto es una variable de clase, se pasa como argumento */
 void* Server::threadListen(void* pData){
 	int sockfd, newsockfd;
 	 socklen_t clilen;
@@ -35,13 +37,10 @@ void* Server::threadListen(void* pData){
 			  error("ERROR on binding");}
 	 listen(sockfd,5);
 	 clilen = sizeof(cli_addr);
-	 newsockfd = accept(sockfd,
-				 (struct sockaddr *) &cli_addr,
-				 &clilen);
+	 newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
 	 if (newsockfd < 0)
-		  error("ERROR on accept");
-
-	while(true){
+		 error("ERROR on accept");
+	 while(true){
 		sleep(0.5);
 
 		bzero(buffer,256);
@@ -56,7 +55,7 @@ void* Server::threadListen(void* pData){
 			break;
 		}
 //		printf("Here is the message: %s\n",buffer);
-		n = write(newsockfd,"I got your message\n ",18);
+		n = write(newsockfd,"Recibi tu mensaje\n ",18);
 	//	if (n < 0)
 	//	 error("ERROR writing to socket");
 
@@ -65,48 +64,6 @@ void* Server::threadListen(void* pData){
 	close(newsockfd);
 	close(sockfd);
 	pthread_exit(NULL);
-}
-
-void Server::conect(){
-	int sockfd, newsockfd, portno;
-			socklen_t clilen;
-			char buffer[256];
-			struct sockaddr_in serv_addr, cli_addr;
-			int n;
-			if (_Port< 2){
-				fprintf(stderr,"ERROR, no port provided\n");
-				exit(1);
-			}
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = _Port;
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd,
-                 (struct sockaddr *) &cli_addr,
-                 &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0)
-    	 error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0)
-    	 error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return;
 }
 
 void Server::error(const char *msg)
--- src/com.HARFS.NetworkAccess/Server.h
+++ src/com.HARFS.NetworkAccess/Server.h
@@ -10,6 +10,13 @@
 
 #include <pthread.h>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 using namespace std;
 
@@ -18,24 +25,11 @@ private:
 	static int _Port;
 public:
 	Server(int pPort);
-	virtual ~Server();
 	static void* threadListen(void* pData);
-	void conect();
 	static void error(const char *msg);
 };
 
 #endif /* NETWORKACCESS_SERVER_H_ */
-
-
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
 
 
